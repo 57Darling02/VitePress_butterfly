7df2488df718a15427a88110da0f89afb3de7295
ZYJiang <3144223912@qq.com>
2025-12-25 14:57:27 +0800

    new: add update_theme.sh

---
+++ update_theme.sh
@@ -0,0 +1,3 @@
+git remote add upstream https://github.com/57Darling02/VitePress_butterfly.git
+
+git fetch upstream && git checkout main && git reset --hard upstream/main && git push origin main --force
